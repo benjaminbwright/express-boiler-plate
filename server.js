df576c1e334c7890ab5a6d8bcc6518433409d334
Ben Wright <bwright@2u.com>
2023-12-20 14:02:12 -0600

    Adds base express server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // DEPENDENCIES
 // express
+const express = require("express");
 
 // DATA
 
 // APP / PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // MIDDLEWARES
 
 // ROUTES
 
 // START SERVER
+app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
